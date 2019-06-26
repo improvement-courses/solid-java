bb9527274b2a1c5165d0b2ff68a3d7f777166f53
Williams Gomes <williamsgomes45@gmail.com>
2019-06-25 23:54:46 -0300

    Acoplamento e a estabilidade

---
+++ src/com/solid/capitulo2/AcaoAposGerarNota.java
@@ -0,0 +1,5 @@
+package com.solid.capitulo2;
+
+public interface AcaoAposGerarNota {
+	void executa(NotaFiscal nf);
+}
---
+++ src/com/solid/capitulo2/semAcoplamento/GeradorDeNotaFiscal.java
@@ -0,0 +1,34 @@
+package com.solid.capitulo2.semAcoplamento;
+
+import java.util.List;
+
+import com.solid.capitulo2.AcaoAposGerarNota;
+import com.solid.capitulo2.Fatura;
+import com.solid.capitulo2.NotaFiscal;
+
+public class GeradorDeNotaFiscal {
+
+	private List<AcaoAposGerarNota> acoes;
+
+    public GeradorDeNotaFiscal(List<AcaoAposGerarNota> acoes) {
+		this.acoes = acoes;
+        
+    }
+
+    public NotaFiscal gera(Fatura fatura) {
+
+        double valor = fatura.getValorMensal();
+
+        NotaFiscal nf = new NotaFiscal(valor, impostoSimplesSobreO(valor));
+
+        for (AcaoAposGerarNota acaoAposGerarNota : acoes) {
+			acaoAposGerarNota.executa(nf);
+		}
+
+        return nf;
+    }
+
+    private double impostoSimplesSobreO(double valor) {
+        return valor * 0.06;
+    }
+}
