df3ab73bd20831ccbd995fffb15fc69857ead0a5
Williams Gomes <williamsgomes45@gmail.com>
2019-06-26 00:44:03 -0300

    Herança e o Liskov Substitutive Principle

---
+++ src/com/solid/capitulo5/ContaDeEstudante.java
@@ -0,0 +1,19 @@
+package com.solid.capitulo5;
+
+public class ContaDeEstudante extends ContaComum {
+	private ManipuladorDeSaldo manipulador;
+	private int milhas;
+
+	public ContaDeEstudante() {
+		this.manipulador = new ManipuladorDeSaldo();
+	}
+
+	public void deposita(double valor) {
+		manipulador.deposita(valor);
+		this.milhas += (int) valor;
+	}
+
+	public int getMilhas() {
+		return milhas;
+	}
+}
